cf6d0d69f6405d06433de9357a045b0433fcc01c
George Flerovsky <george.flerovsky@canada.ca>
2020-03-11 10:29:07 -0400

    Added property tests for dbWritetable. 6/9 failed. - A dataframe without logical columns breaks the pipeline.

--- tests/testthat/test-property.R
+++ tests/testthat/test-property.R
@@ -1,10 +1,19 @@
 library(hedgehog)
 
+# ====================================
+# Generators
+
+# Generate 1 to 26 factor levels as unique single characters.
+gen_factor_levels <- gen.with(
+  gen.int(26),
+  function(m) letters[1:m]
+)
+
 # Generate a random-leveled ordered/unordered factor column with `n` values.
 gen_factor <- function(n) gen.and_then(
-  gen.and_then(gen.int(26), function(m) gen.pure(letters[1:m])),
+  gen_factor_levels,
   function (levels) gen.map(
-    function(args) do.call(factor,args),
+    function(args) do.call(factor, args),
     list(
       x = gen.c(gen.element(levels), of = n),
       levels = levels,
@@ -30,6 +39,140 @@ gen_df <- gen.with(
   function(cols) do.call(dplyr::tibble, rlang::set_names(cols, letters[1:(length(cols))]))
 )
 
+# ====================================
+# Generator Tests
 test_that("All generated tibbles have length 5.",
-  forall(gen_df, function(df) {expect_equal(nrow(df), 5)}, tests = 100)
-)
+  forall(gen_df, tests = 100, function(df) {
+    expect_equal(nrow(df), 5)
+}))
+
+# ====================================
+# dbwritetable2 tests
+test_that('.process_logicals is character.',
+  forall(gen_df, function(df) {
+    expect_type(.process_logicals(df, 'test-name'), 'character')
+}))
+
+test_that('.process_logicals length is multiple of 4.',
+  forall(gen_df, function(df) {
+    object <- .process_logicals(df, 'test-name')
+    remainder <- length(object) %% 4
+    expect_equal(remainder, 0)
+}))
+
+test_that('.process_factors is a list',
+  forall(gen_df, function(df) {
+    expect_type(.process_factors(df, 'test-name'), 'list')
+}))
+
+test_that('.process_factors length is equal to factor elems of df.',
+  forall(gen_df, function(df) {
+    object <- .process_factors(dt, 'test-name')
+    ix <- 0
+    for (col in df) {
+      if (is.factor(col)) ix <- ix + 1
+    }
+    expect_length(object, ix)
+}))
+
+# ====================================
+# dbwritetable2 tests
+
+test_that('.create_meta returns the correctly formatted tibble.',
+  forall(gen_df, function(df) {
+    tbl <- .create_meta(
+      .process_logicals(dt, 'test'), .process_factors(dt, 'test')
+    )
+    tbl_check <- tibble::tribble(
+      ~table, ~var, ~type, ~levels,
+      'test', 'b', 'logical', '',
+      'test', 'd', 'logical', '',
+      'test', 'e', 'factor', jsonlite::toJSON(list(
+        ordered = FALSE, levels = levels(dt$e)
+      ), auto_unbox = TRUE),
+      'test', 'f', 'factor', jsonlite::toJSON(list(
+        ordered = TRUE, levels = levels(dt$f)
+      ), auto_unbox = TRUE)
+    )
+    expect_identical(tbl, tbl_check)
+}))
+
+test_that('.create_or_update_rtypes creates __types table if not exist.',
+  forall(gen_df, function(df) {
+    meta <- .create_meta(
+      .process_logicals(dt, 'test'), .process_factors(dt, 'test')
+    )
+
+    db_table <- tibble::tribble(
+      ~table, ~var, ~type, ~levels,
+      'test', 'b', 'logical', '',
+      'test', 'd', 'logical', '',
+      'test', 'e', 'factor', jsonlite::toJSON(list(
+        ordered = FALSE, levels = levels(dt$e)
+      ), auto_unbox = TRUE),
+      'test', 'f', 'factor', jsonlite::toJSON(list(
+        ordered = FALSE, levels = levels(dt$f)
+      ), auto_unbox = TRUE),
+      'test2', 'ab', 'logical', ''
+    )
+
+    con <- DBI::dbConnect(RSQLite::SQLite(), ':memory:')
+    .create_or_update_rtypes(con, meta)
+    expect_true(DBI::dbExistsTable(con, '__types'))
+    DBI::dbDisconnect(con)
+}))
+
+test_that('.create_or_update_rtypes updates correctly.',
+  forall(gen_df, function(df) {
+    meta <- .create_meta(
+      .process_logicals(dt, 'test'), .process_factors(dt, 'test')
+    )
+
+    db_table <- tibble::tribble(
+      ~table, ~var, ~type, ~levels,
+      'test', 'b', 'logical', '',
+      'test', 'd', 'logical', '',
+      'test', 'e', 'factor', jsonlite::toJSON(list(
+        ordered = FALSE, levels = levels(dt$e)
+      ), auto_unbox = TRUE),
+      'test', 'f', 'factor', jsonlite::toJSON(list(
+        ordered = FALSE, levels = levels(dt$f)
+      ), auto_unbox = TRUE),
+      'test2', 'ab', 'logical', ''
+    )
+
+    con <- DBI::dbConnect(RSQLite::SQLite(), ':memory:')
+    DBI::dbCreateTable(con, '__types', db_table)
+    DBI::dbAppendTable(con, '__types', db_table)
+    .create_or_update_rtypes(con, meta)
+
+    rtypes <- dplyr::tbl(con, '__types') %>%
+      dplyr::collect() %>%
+      dplyr::arrange(table, var)
+
+    # Update should now render factor as ordered. The earilier entries should
+    # also be retained.
+    db_table_expect <- db_table %>%
+      dplyr::arrange(table, var) %>%
+      dplyr::mutate(
+        levels = ifelse(
+          table == 'test' & var == 'f',
+          jsonlite::toJSON(
+            list(ordered = TRUE, levels = levels(dt$f)), auto_unbox = TRUE
+          ),
+          levels
+        )
+      )
+
+    expect_identical(rtypes, db_table_expect)
+    DBI::dbDisconnect(con)
+}))
+
+test_that('dbWriteTable2 produces __types with proper roundtrip info.',
+  forall(gen_df, function(df) {
+    con <- DBI::dbConnect(RSQLite::SQLite(), ':memory:')
+    dbWriteTable2(con, 'test_table', dt)
+    tbl <- collect_with_types(dplyr::tbl(con, 'test_table'))
+    expect_identical(dt, tbl)
+    DBI::dbDisconnect(con)
+}))
